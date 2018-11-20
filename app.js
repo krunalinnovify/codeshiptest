d174cbff2f6221572db9df421bd559582cf77d5e
Krunal Babaria <krunal.b@innovify.in>
2018-11-20 15:21:25 +0530

    update files

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 7006
+const port = 3000
 
 app.get('/', (req, res) => res.send('Hello I am Krunal Babaria from innovify!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
